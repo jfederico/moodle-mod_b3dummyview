5f901fc447d224782cb15a01d172eade26e7f71f
jfederico <jesus@123it.ca>
2025-09-25 18:26:02 +0000

    added support for mobile app

---
+++ classes/output/mobile.php
@@ -0,0 +1,51 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+namespace mod_b3dummyview\output;
+
+use stdClass;
+
+/**
+ * Mobile support for mod_b3dummyview
+ *
+ * @package    mod_b3dummyview
+ * @copyright  2025 Jesus Federico <jesus@123it.ca>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+class mobile {
+    /**
+     * Returns the data and template for the mobile app view.
+     *
+     * @param array $args
+     * @return array
+     */
+    public static function mobile_view($args) {
+        $data = [
+            'message' => 'Hello Mobile from b3dummyview::renderer!'
+        ];
+        return [
+            'templates' => [
+                [
+                    'id' => 'main',
+                    'html' => '<ion-card><ion-card-content>{{message}}</ion-card-content></ion-card>'
+                ]
+            ],
+            'javascript' => '',
+            'otherdata' => $data,
+            'files' => []
+        ];
+    }
+}
---
+++ db/mobile.php
@@ -0,0 +1,42 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+/**
+ * Mobile support for mod_b3dummyview
+ *
+ * @package    mod_b3dummyview
+ * @copyright  2025 Jesus Federico <jesus@123it.ca>
+ * @license    http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$addons = [
+    "mod_b3dummyview" => [
+        "handlers" => [
+            "b3dummyview" => [
+                "displaydata" => [
+                    "title" => "b3dummyview",
+                    "icon" => "$CFG->wwwroot/mod/b3dummyview/pix/icon.svg",
+                    "class" => "mod_b3dummyview_mobile_handler"
+                ],
+                "method" => "mobile_view",
+                "offlinefunctions" => [
+                    "mobile_view" => []
+                ]
+            ]
+        ],
+        "lang" => ["pluginname", "modulename"]
+    ]
+];
